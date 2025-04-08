fd5ca2d24bf3ff91e275a3f21bd8de30e0465007
Kyrylko <kkuzmenko1992@gmail.com>
2025-04-08 15:52:01 -0230

    test styles for mappage

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -1,12 +1,14 @@
 import { lazy, Suspense } from "react";
 
+import styles from "./MapPage.module.css";
+
 const MapView = lazy(() => import("../../components/MapView/MapView"));
 
 const MapPage = () => {
 
 
   return (
-    <div>
+    <div className={styles.pageWrapper}>
       <h2>Map</h2>
       <Suspense fallback={<div>Loading map...</div>}>
         <MapView />
